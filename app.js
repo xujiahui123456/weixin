b65109056ce1def9126d955bd82809d00940b184
徐家汇 <1504881098@qq.com>
2020-04-29 19:38:29 +0800

    初始小程序

---
+++ app.js
@@ -0,0 +1,47 @@
+//app.js
+console.log('================');
+// 1.小程序不是运行在浏览器运行在浏览器中，所有没有BOM和DOM对象
+// console.log(window);  => undefined
+// console.log(document); => undefined
+console.log('================');
+App({
+  onLaunch: function () {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
+
+// 调用APP方法的作用就是创建应用程序实例对象
+// 定义应用程序的生命周期事件
